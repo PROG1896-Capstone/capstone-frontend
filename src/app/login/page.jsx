d7534ce804cca6f6ad8f246d7a116c6c9d53ff09
VasuBeachoo <vasubeachoo@gmail.com>
2023-06-12 18:42:36 -0400

    Create and style Login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,43 @@
+import Link from "next/link";
 import styles from "./login.module.css";
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.page}>
+      <form className={styles.form}>
+        <h1 className={styles.heading}>Login</h1>
+        <div className={styles.input_box}>
+          <label className={styles.label} htmlFor="login_email">
+            Your Email
+          </label>
+          <input
+            className={styles.input}
+            type="email"
+            name="email"
+            id="login_email"
+          />
+        </div>
+        <div className={styles.input_box}>
+          <label className={styles.label} htmlFor="login_password">
+            Password
+          </label>
+          <input
+            className={styles.input}
+            type="password"
+            name="password"
+            id="login_password"
+          />
+        </div>
+        <input className={styles.submit} type="submit" value="Login" />
+        <Link className={styles.link} href="/">
+          Forgot Password?
+        </Link>
+        <Link className={styles.link} href="/">
+          Don't have an account?
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
