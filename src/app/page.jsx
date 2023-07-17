c8e43fa0002c846912ef2de279011d26faa55b9f
VasuBeachoo <vasubeachoo@gmail.com>
2023-07-17 18:56:44 -0400

    Update home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,15 +3,12 @@ import styles from "./home.module.css";
 export const Home = () => {
   return (
     <div className={styles.page}>
-      <div className={styles.page_container}>
+      <div className={styles.container}>
         <h1 className={styles.heading} id={styles.title}>
           Sunset Bistro
         </h1>
-        <div className={styles.container}>
-          <h1 className={styles.heading}>Hungry?</h1>
-          <p className={styles.paragraph}>Once you try it, you will love it.</p>
-          <button className={styles.btn}>Order Now</button>
-        </div>
+        <p className={styles.paragraph}>Once you try it, you will love it.</p>
+        <button className={styles.btn}>Order Now</button>
       </div>
     </div>
   );
