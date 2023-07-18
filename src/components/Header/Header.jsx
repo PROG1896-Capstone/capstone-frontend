d35953e76e6a9c521a623a2c7b4f5d6094407fcc
VasuBeachoo <vasubeachoo@gmail.com>
2023-07-17 21:52:28 -0400

    Add option to collapse mobile nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import iconLogo from "@/assets/sunset-bistro-logo.png";
+import iconMenuOpen from "@/assets/icon-menu-open.svg";
+import iconMenuClose from "@/assets/icon-menu-close.svg";
 import iconCart from "@/assets/icon-cart.svg";
 import iconAccount from "@/assets/icon-account.svg";
 import styles from "./header.module.css";
@@ -38,25 +42,50 @@ export const Header = () => {
     },
   ];
 
+  const [menuOpen, setMenuOpen] = useState(true);
+
+  const handleResize = () => {
+    if (window.innerWidth > 550) setMenuOpen(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <header className={styles.header}>
-      <Image className={styles.logo} src={iconLogo} alt="sunset-bistro-logo" />
-      <nav className={styles.nav}>
-        <div className={styles.text_links}>
-          {textLinks.map((link) => (
-            <Link className={styles.link} href={link.href}>
-              {link.label}
-            </Link>
-          ))}
-        </div>
-        <div className={styles.icon_links}>
-          {iconLinks.map((link) => (
-            <Link className={styles.link} href={link.href}>
-              {link.label}
-            </Link>
-          ))}
-        </div>
-      </nav>
+      <div className={styles.logo_container}>
+        <Image
+          className={styles.logo}
+          src={iconLogo}
+          alt="sunset-bistro-logo"
+        />
+        <Image
+          className={styles.menu_icon}
+          src={menuOpen ? iconMenuClose : iconMenuOpen}
+          alt={menuOpen ? "icon-menu-close" : "icon-menu-open"}
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+      </div>
+      {menuOpen && (
+        <nav className={styles.nav}>
+          <div className={styles.text_links}>
+            {textLinks.map((link) => (
+              <Link className={styles.link} href={link.href}>
+                {link.label}
+              </Link>
+            ))}
+          </div>
+          <div className={styles.icon_links}>
+            {iconLinks.map((link) => (
+              <Link className={styles.link} href={link.href}>
+                {link.label}
+              </Link>
+            ))}
+          </div>
+        </nav>
+      )}
     </header>
   );
 };
